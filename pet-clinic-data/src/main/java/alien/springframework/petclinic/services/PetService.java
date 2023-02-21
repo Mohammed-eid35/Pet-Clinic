4c57461c0aa42e45bed1003bc3858d68ec9a95e9
Muhammad Eid <meid95874@gmail.com>
2023-02-21 16:54:53 +0200

    Implementing Interfaces for services.

---
+++ pet-clinic-data/src/main/java/alien/springframework/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package alien.springframework.petclinic.services;
+
+import alien.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/alien/springframework/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package alien.springframework.petclinic.services;
+
+import alien.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
